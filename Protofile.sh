e208cf8d8a6c02f23d92c87cdaa04be4ca73dbd0
salehmu <ghd@keemail.me>
2022-12-13 19:38:40 +0200

    Deprecate Zalh

--- Protofile.sh
+++ Protofile.sh
@@ -8,12 +8,6 @@ protoc --go_out=. --go_opt=paths=import    \
     protobufs/$result/*
 
 
-
-protoc --go_out=. --go_opt=paths=import    \
-    --go-grpc_out=. --go-grpc_opt=paths=import \
-    protobufs/zalh/*
-
-
 protoc --go_out=. --go_opt=paths=import    \
     --go-grpc_out=. --go-grpc_opt=paths=import \
     protobufs/mamar/*
